6d9401ba1f17af75affb1429592c6dfda5300e0c
energyguy <shahidhassim@gmail.com>
2016-09-18 18:44:58 +0000

    R programming Assignment 2 - Week3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function makeCacheMatrix creates a square matrix.
+## The cacheSolve function computes the inverse matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix reads in a list of values and the number of rows(and columns)
+## The function then forms a square matrix, which is stored in its environment,
+## together with the get, set , getinverse and setinverse functions which is used 
+## in the cacheSolve function.
+##
+## Please use the following commands to call the functions.
+## Ensure that the amount of numerical values is a square number. Inverse
+## Matrices are only performed on square matrices.
+## mcm<-makeCacheMatrix(A<-c(2,4,10,12),n<-sqrt(length(A)))
+## cacheSolve(mcm)
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(A,n) {
+  Ai <- NULL
+ #1: get input values - matrix  
+  set <- function(y) {  
+    Ai <<- NULL
+    A <<- y
+    Ai <<- NULL
+  } 
+     get <- function() A<-matrix(A,nrow=n,ncol=n)
+  setinverse <-function(solve)
+    Ai<<-solve
+  getinverse <-function()
+    Ai
+    list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
+## The cachesolve function first checks for the presence of the inverse matrix.
+## If present it retreives it from cache. Else it computes it.
+## The purpose of this is to save computational time which is quite beneficial 
+## when procession large amounts of data.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(A,...){
+ Ai<-A$getinverse()
+ if(!is.null(Ai)) {
+   message("getting cached data")
+   return(Ai)
+    }
+   data <- A$get()
+   Ai <- solve(data, ...)
+  A$setinverse(Ai)
+  Ai
 }
